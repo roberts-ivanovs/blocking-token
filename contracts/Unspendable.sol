560acbed395b9872f99ac71cc4d5445e77a5ff04
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2021-06-18 09:53:56 +0300

    Sol: Implement token buying with ether

--- contracts/Unspendable.sol
+++ contracts/Unspendable.sol
@@ -1,21 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-// import "./erc20-utils/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Unspendable is ERC20 {
+contract Unspendable is ERC20, Ownable {
 
     struct FrozenTokens {
         uint224 amount;
         uint32 blockNumber;
     }
-    // `value` represents the mapping of `block ID` -> `frozen funds`
+    // Track uers and their frozen funds
     mapping(address => FrozenTokens) private _volitalteFrozen;
+    // The price per each ERC20 token
+    uint256 private _weiPerToken;
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 100 * 10**uint256(decimals()));
+        _mint(address(this), 100 * 10**uint256(decimals())); // People will buy these tokens
+        _weiPerToken = 1000;
     }
+
+    // ---------------------- Internal logic ----------------------
 
     /**
      * @dev Hook that is called before any transfer of tokens. This includes
@@ -55,8 +61,8 @@ contract Unspendable is ERC20 {
                 "Cannot transfer at the same transaction as when receiving!"
             );
         }
-        // Execute always (incl. minting)
-        if (to != address(0)) {
+        // Execute always (incl. minting), except when minting `address(this)`
+        if (to != address(0) && to != address(this)) {
             if (_volitalteFrozen[to].blockNumber == _castBlockNumber) {
                 // NOTE: This can overflow!
                 _volitalteFrozen[to].amount += _castAmoint;
@@ -65,5 +71,35 @@ contract Unspendable is ERC20 {
             }
             _volitalteFrozen[to].blockNumber = _castBlockNumber;
         }
+    }
+
+    // ---------------------- Public interface ----------------------
+
+    /* Set the new price per token
+     */
+    function setTokenRateInWei(uint256 _newPrice) public onlyOwner {
+        _weiPerToken = _newPrice;
+    }
+
+    /* Increase the total supply of tokens for the address of THIS contract
+     */
+    function increaseStorageReserves(uint256 _deltaTokens) public onlyOwner {
+        _mint(address(this), _deltaTokens);
+    }
+
+    /* Send all of the ether stored by this contract to the owner
+     */
+    function grabEther() public onlyOwner {
+        payable(this.owner()).transfer(address(this).balance);
+    }
+
+
+    /* Buy tokens for someone (or yourself)
+     */
+    function buyTokensForAddress(address _tokenReceiver) public payable {
+        uint256 tokensBeingBough = msg.value / _weiPerToken;
+        // Using up the contracts own supply.
+        // Will throw if not enoughs tokens available.
+        this.transfer(_tokenReceiver, tokensBeingBough);
     }
 }
