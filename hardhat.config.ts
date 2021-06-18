d1034aa84fc45a740ff818d2d9a51512e0e10dfa
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2021-06-18 14:47:41 +0300

    Tests: hardhat will genereate Type annotations for contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,12 @@
-import "@nomiclabs/hardhat-ethers";
-import "hardhat-gas-reporter"
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-gas-reporter';
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
+import { HardhatUserConfig } from 'hardhat/config';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.4',
 };
+
+export default config;
